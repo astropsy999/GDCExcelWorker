bae5a1217b09428f63b44efaf3e3109ffc698030
StuzhukEV <stuzhukev@giapdc.ru>
2024-04-22 15:24:42 +0300

    Transition to xlwings in oen branch

--- excel_processing.py
+++ excel_processing.py
@@ -1,9 +1,5 @@
 import os
 import logging
-from openpyxl import load_workbook
-from openpyxl.worksheet.worksheet import Worksheet
-from openpyxl.cell import MergedCell
-import re
 from datetime import datetime
 import shutil
 from process_excel_rgf import process_excel_file_rgf
@@ -42,6 +38,8 @@ def process_all_files(directory, installation_name, control_date, option):
     # Логирование итогов
     logging.info(f'Обработано {processed_count} файлов.')
     
+    return
+    
     # Создаем папку для сохранения обработанных файлов
     current_date = datetime.now().strftime("%d-%m-%Y")
     output_directory = f"Обработано_{current_date}"
@@ -71,152 +69,6 @@ def process_all_files(directory, installation_name, control_date, option):
         logging.info(f'ГОТОВЫЕ ФАЙЛЫ СОХРАНЕНЫ в новой папке "{output_directory}".')
 
     return processed_count
-
-# def process_excel_file(file_path, installation_name, control_date):
-#     # Загрузите файл
-#     workbook = load_workbook(file_path)
-
-#     file_name = os.path.basename(file_path)
-
-#     # Лист 'Характеристики'
-#     characteristics_sheet = workbook['Характеристики']
-
-#     # Установка значения в C2
-#     characteristics_sheet['C2'].value = installation_name
-#     logging.info(f'----- НАЧАЛО ОБРАБОТКИ ФАЙЛА: {file_name} -----"')
-#     logging.info(f'Установлено значение {installation_name} в ячейку C2 на листе "Характеристики"')
-
-#     # Проверка ячейки A1 и установка значения в C4
-#     a1_value = characteristics_sheet['A1'].value
-#     c4_value = None  # Значение по умолчанию для C4
-
-#     if a1_value:
-#         c4_value = a1_value
-#         logging.info(f'Значение {a1_value} из ячейки A1 скопировано в ячейку C4 на листе "Характеристики"')
-#     else:
-#         logging.info(f'Ячейка A1 пуста, ячейка C4 будет установлена в None')
-
-#     characteristics_sheet['C4'].value = c4_value
-
-#     # Лист 'Диагностическая карта'
-#     diagnostics_sheet = workbook['Диагностическая карта']
-
-#     # Установка даты контроля в L5
-#     diagnostics_sheet['L5'].value = control_date
-#     logging.info(f'Установлено значение {control_date} в ячейку L5 на листе "Диагностическая карта"')
-
-#     # Счетчик измененных строк в столбце A
-#     changed_rows_count = 0
-
-#     # Добавление формулы в столбец A
-#     # Начинаем с первой строки данных (возможно, первая строка - это заголовок)
-#     for row in range(8, diagnostics_sheet.max_row + 1):
-#         # Формула для конкатенации B и C с точкой
-#         formula = f'=CONCATENATE(B{row}, ".", C{row}, ".")'
-
-#         # Проверка, что ячейки B{row} и C{row} не пусты перед установкой формулы в A{row}
-#         if diagnostics_sheet[f'B{row}'].value is not None and diagnostics_sheet[f'C{row}'].value is not None:
-#             diagnostics_sheet[f'A{row}'].value = formula
-#             changed_rows_count += 1  # Увеличиваем счетчик для каждой измененной строки
-
-#     # Логирование количества измененных строк
-#     logging.info(f'Количество строк, где была установлена формула в столбец A: {changed_rows_count}')
-
-#     # Сохранение файла
-#     workbook.save(file_path)
-
-#     # Логирование успешной обработки файла
-#     logging.info(f'ФАЙЛ {file_name} ОБРАБОТАН УСПЕШНО!.')
-
-#     # Загрузите файл
-#     workbook = load_workbook(file_path)
-
-#     # Лист 'Характеристики'
-#     characteristics_sheet = workbook['Характеристики']
-
-#     # Установка значения в C2
-#     characteristics_sheet['C2'].value = installation_name
-
-#     # Проверка ячейки A1 и установка значения в C4
-#     if characteristics_sheet['A1'].value:
-#         characteristics_sheet['C4'].value = characteristics_sheet['A1'].value
-#     else:
-#         characteristics_sheet['C4'].value = None
-
-#     # Лист 'Диагностическая карта'
-#     diagnostics_sheet = workbook['Диагностическая карта']
-
-#     # Установка даты контроля в L5
-#     diagnostics_sheet['L5'].value = control_date
-
-#     # Сохранение файла
-#     workbook.save(file_path)
-
-#     # Логирование успешной обработки файла
-#     # logging.info(f'Файл {file_path} обработан успешно.')
-
-# def process_excel_file_oen(file_path, installation_name, control_date):
-#     # Открываем файл Excel
-#     workbook = load_workbook(file_path)
-
-#     # Доступ к листу "УЗТ (коркарта)"
-#     sheet = workbook['УЗТ (коркарта)']
-
-#     # Проверка ячейки L5
-#     cell_L5 = sheet['L5']
-#     if cell_L5.value:
-#         # Добавляем новый столбец перед B
-#         sheet.insert_cols(2)
-
-#         # Добавляем формулу в столбец B
-#         for row in range(2, sheet.max_row + 1):
-#             # Получаем ячейку в столбце 2
-#             cell = sheet.cell(row=row, column=2)
-#             # Проверяем, является ли ячейка объединенной
-#             if isinstance(cell, MergedCell):
-#                 # Найдем верхнюю левую ячейку объединенного диапазона
-#                 top_left_cell = sheet.merged_cells.ranges.get(cell.coordinate)
-#                 if top_left_cell:
-#                     # Устанавливаем формулу в верхней левой ячейке
-#                     top_left_cell.value = f'=CONCATENATE(B{row}; "."; C{row}; ".")'
-#             else:
-#                 # Устанавливаем значение, если ячейка не объединена
-#                 cell.value = f'=CONCATENATE(B{row}; "."; C{row}; ".")'
-
-#     # Вставляем введенное значение в ячейку F7
-#     sheet['F7'].value = installation_name
-
-#     # Проверка значения в ячейке F7 на соответствие формату ХХ.ХХ.ХХХХ
-#     cell_value = sheet['F7'].value
-#     pattern = re.compile(r'^\d{2}\.\d{2}\.\d{4}$')
-#     if not pattern.match(cell_value):
-#         # Если значение не соответствует формату, удаляем "г." из даты
-#         sheet['F7'].value = cell_value.replace("г.", "")
-
-#     # Сохраняем файл
-#     workbook.save(file_path)
-
-#     return f"Обработано успешно! Файл сохранен: {file_path}"
-
-# def set_value_if_not_merged(sheet: Worksheet, row: int, col: int, value):
-#     cell = sheet.cell(row=row, column=col)
-#     merged_ranges = sheet.merged_cells.ranges
-
-#     # Проверяем, находится ли ячейка в объединенном диапазоне
-#     if any(range.min_row <= cell.row <= range.max_row and
-#            range.min_col <= cell.column <= range.max_col
-#            for range in merged_ranges):
-#         # Находим верхний левый угол объединенного диапазона
-#         for range in merged_ranges:
-#             if range.min_row <= cell.row <= range.max_row and \
-#                range.min_col <= cell.column <= range.max_col:
-#                 # Устанавливаем значение в верхней левой ячейке
-#                 top_left_cell = sheet.cell(row=range.min_row, column=range.min_col)
-#                 top_left_cell.value = value
-#                 break
-#     else:
-#         # Устанавливаем значение, если ячейка не объединена
-#         cell.value = value
 
 
 
--- process_excel_oen.py
+++ process_excel_oen.py
@@ -1,67 +1,125 @@
 import logging
-from openpyxl import load_workbook
-from openpyxl.worksheet.worksheet import Worksheet
-from openpyxl.cell import MergedCell
+import os
 import re
+from openpyxl import load_workbook
+from openpyxl.cell.cell import MergedCell
+import xlwings as xw
+
 def process_excel_file_oen(file_path, installation_name, control_date):
     # Открываем файл Excel
-    workbook = load_workbook(file_path)
+    # workbook = load_workbook(file_path)
+    # logging.info(f'----- НАЧАЛО ОБРАБОТКИ ФАЙЛА: {os.path.basename(file_path)} -----')
+    # logging.info(f'Загружен файл Excel: {file_path}')
+    
+    # # Доступ к листу "УЗТ (коркарта)"
+    # sheet = workbook['УЗТ (коркарта)']
+    # logging.info('Доступ к листу "УЗТ (коркарта)"')
+
+    # # Получаем ячейку L5
+    # cell_L5 = sheet['L5']
+
+    # # Получаем значение ячейки L5
+    # value_L5 = cell_L5.value
+    # logging.info(f'Значение ячейки L5: {value_L5}')
+
+    # # Если значение в ячейке L5 является формулой, извлекаем адрес ячейки, на которую она ссылается
+    # if isinstance(value_L5, str) and value_L5.startswith('='):
+    #     sheet.insert_cols(2)
+    #     logging.info(f'ДОБАВЛЯЕМ НОВЫЙ СТОЛБЕЦ ПЕРЕД B!')
+
+    # else:
+    #     # Если L5 не содержит формулы или значение равно None
+    #     # Проверяем, скрыт ли столбец A
+    #     if sheet.column_dimensions['A'].hidden:
+    #         logging.info(f'Cтолбец A скрыт!')
+    #         # Показываем столбец A
+    #         sheet.column_dimensions['A'].hidden = False
+    #         logging.info(f'Показываем столбец A')
+    
+    # # Сохраняем файл
+    # workbook.save(file_path)
+    # logging.info(f'Файл {file_path} успешно сохранен')        
+       
+      # Открываем книгу Excel с помощью xlwings
+    app = xw.App(visible=False)  # Запускаем приложение Excel в фоновом режиме
+    workbook = app.books.open(file_path)
+    logging.info(f'----- НАЧАЛО ОБРАБОТКИ ФАЙЛА: {os.path.basename(file_path)} -----')
+    logging.info(f'Загружен файл Excel: {file_path}')
 
     # Доступ к листу "УЗТ (коркарта)"
-    sheet = workbook['УЗТ (коркарта)']
+    sheet = workbook.sheets['УЗТ (коркарта)']
+    logging.info('Доступ к листу "УЗТ (коркарта)"')
 
-    # Проверка ячейки L5
-    cell_L5 = sheet['L5']
-    if cell_L5.value:
-        # Добавляем новый столбец перед B
-        sheet.insert_cols(2)
+    # Получаем ячейку L5
+    cell_L5 = sheet.range('L5')
 
-        # Добавляем формулу в столбец B
-        for row in range(2, sheet.max_row + 1):
-            # Получаем ячейку в столбце 2
-            cell = sheet.cell(row=row, column=2)
-            # Проверяем, является ли ячейка объединенной
-            if isinstance(cell, MergedCell):
-                # Найдем верхнюю левую ячейку объединенного диапазона
-                top_left_cell = sheet.merged_cells.ranges.get(cell.coordinate)
-                if top_left_cell:
-                    # Устанавливаем формулу в верхней левой ячейке
-                    top_left_cell.value = f'=CONCATENATE(B{row}; "."; C{row}; ".")'
-            else:
-                # Устанавливаем значение, если ячейка не объединена
-                cell.value = f'=CONCATENATE(B{row}; "."; C{row}; ".")'
+    # Получаем значение ячейки L5
+    value_L5 = cell_L5.value
+    logging.info(f'Значение ячейки L5: {value_L5}')
 
-    # Вставляем введенное значение в ячейку F7
-    sheet['F7'].value = installation_name
+    # Если значение в ячейке L5 является формулой, вставляем новый столбец перед B
+    if isinstance(value_L5, str) and value_L5.startswith('='):
+        sheet.api.Columns("B").Insert()
+        logging.info(f'ДОБАВЛЯЕМ НОВЫЙ СТОЛБЕЦ ПЕРЕД B!')
+    else:
+        # Если L5 не содержит формулы или значение равно None
+        # Проверяем, скрыт ли столбец A
+        if sheet.range('A1').api.EntireColumn.Hidden:
+            logging.info(f'Cтолбец A скрыт!')
+            # Показываем столбец A
+            sheet.range('A1').api.EntireColumn.Hidden = False
+            logging.info(f'Показываем столбец A')
 
-    # Проверка значения в ячейке F7 на соответствие формату ХХ.ХХ.ХХХХ
-    cell_value = sheet['F7'].value
-    pattern = re.compile(r'^\d{2}\.\d{2}\.\d{4}$')
-    if not pattern.match(cell_value):
-        # Если значение не соответствует формату, удаляем "г." из даты
-        sheet['F7'].value = cell_value.replace("г.", "")
+    # Обновляем формулы на листе, чтобы учесть изменения
+    sheet.api.Calculate()
+    logging.info('Обновили формулы на листе после изменений')
 
     # Сохраняем файл
     workbook.save(file_path)
+    logging.info(f'Файл {file_path} успешно сохранен')
 
-    return f"Обработано успешно! Файл сохранен: {file_path}"
+    # Закрываем книгу Excel
+    workbook.close()
+    app.quit()     
+            
+    return
+    
+    # if type(value_L5) == 'string':
+    pattern = re.compile(r'^\d{2}\.\d{2}\.\d{4}$')
+    logging.info(f'Проверка значения в ячейке F5: {value_L5}')
+    if not pattern.match(value_L5):
+            logging.warning(f'Значение в ячейке F5 не соответствует формату DD.MM.YYYY. Значение: {value_L5}')
+            sheet['F7'].value = cell_value.replace("г.", "")
+                
+            new_value = sheet['F7'].value
+            logging.info(f'Удалено "г." из ячейки F7, новое значение: {new_value}')
+            return 
+                # Добавляем новый столбец перед B
+            sheet.insert_cols(2)
+            logging.info('Добавлен новый столбец перед B')
 
-def set_value_if_not_merged(sheet: Worksheet, row: int, col: int, value):
-    cell = sheet.cell(row=row, column=col)
-    merged_ranges = sheet.merged_cells.ranges
+        # Добавляем формулу в столбец B
+            for row in range(2, sheet.max_row + 1):
+                cell = sheet.cell(row=row, column=2)
+                if isinstance(cell, MergedCell):
+                    top_left_cell = sheet.merged_cells.ranges.get(cell.coordinate)
+                    if top_left_cell:
+                        top_left_cell.value = f'=CONCATENATE(B{row}; "."; C{row}; ".")'
+                else:
+                    cell.value = f'=CONCATENATE(B{row}; "."; C{row}; ".")'
+                logging.info(f'Установлена формула в B{row}')
+                
+    return 
+    # Вставляем введенное значение в ячейку F7
+    sheet['F7'].value = installation_name
+    logging.info(f'Установлено значение "{installation_name}" в ячейку F7')
 
-    # Проверяем, находится ли ячейка в объединенном диапазоне
-    if any(range.min_row <= cell.row <= range.max_row and
-           range.min_col <= cell.column <= range.max_col
-           for range in merged_ranges):
-        # Находим верхний левый угол объединенного диапазона
-        for range in merged_ranges:
-            if range.min_row <= cell.row <= range.max_row and \
-               range.min_col <= cell.column <= range.max_col:
-                # Устанавливаем значение в верхней левой ячейке
-                top_left_cell = sheet.cell(row=range.min_row, column=range.min_col)
-                top_left_cell.value = value
-                break
-    else:
-        # Устанавливаем значение, если ячейка не объединена
-        cell.value = value
+    # Проверка значения в ячейке F7 на соответствие формату DD.MM.YYYY
+    cell_value = sheet['F7'].value
+   
+
+    # Сохраняем файл
+    workbook.save(file_path)
+    logging.info(f'Файл {file_path} успешно сохранен')
+
+    return f'Обработано успешно! Файл сохранен: {file_path}'
